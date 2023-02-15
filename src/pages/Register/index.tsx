548258b71d3e3dfdbea76fd4fdd0797207307271
Jason <jday@nexrage.com>
2023-02-15 11:52:34 -0600

    mobile friendly

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,101 +1,125 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'components/ui/Inputs/Button';
 
-export const Register = () => {
-  
-	return (
-		<>
-			{/* master view */}
-			<div id='AuthScreenRegister' style={{ height: '90vh' }}>
-				{/* background image */}
-				<div className=' flex h-full max-w-full flex-row rounded-3xl border-2 bg-gradient-to-br from-vendorfiedBlue/[.1] to-vendorfiedGreen/[.1] bg-center'>
-					{/* column 1 */}
-					<div className='flex flex-1 flex-col items-center justify-center'>
-						<h1 className='mb-4 text-3xl font-bold'>Welcome</h1>
-						<h1 className='mb-4 text-5xl font-bold'>Select your Vendorfied plan</h1>
-						<p className='p-9 text-center text-lg text-grey'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare
-							proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor sit amet,
-							consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin adipiscing amet.
-							Euismod elit, vel tellus, sed in sapien.
-						</p>
-						<details>
-							<summary className='mb-4 text-2xl font-bold hover:cursor-pointer'>How do I cancel my subscription?</summary>
-							<div className='mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400'>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
-									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien.
-								</p>
-							</div>
-						</details>
-						<details>
-							<summary className='mb-4 text-2xl font-bold hover:cursor-pointer'>How do I cancel my subscription?</summary>
-							<div className='mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400'>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
-									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien.
-								</p>
-							</div>
-						</details>
-						<details>
-							<summary className='mb-4 text-2xl font-bold hover:cursor-pointer'>How do I cancel my subscription?</summary>
-							<div className='mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400'>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
-									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien.
-								</p>
-							</div>
-						</details>
-					</div>
-					{/* column 2 */}
-					<div className='flex flex-1 flex-col items-center justify-center gap-4 '>
-						<div className='flex w-1/2 items-center gap-4 rounded-xl bg-white p-9 border-blue border-3 hover:cursor-pointer '>
-							<input type='checkbox' />
-							<div>
-								<h2 className='font-size p-1 text-xl text-4xl font-bold'>Starter</h2>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
-									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor
-									sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin
-									adipiscing amet. Euismod elit, vel tellus, sed in sapien..
-								</p>
-							</div>
-							<p>$20/mo.</p>
-						</div>
-						<div className='flex w-1/2 items-center gap-4 rounded-xl bg-white p-9 hover:cursor-pointer '>
-							<input type='checkbox' />
-							<div>
-								<h2 className='font-size p-1 text-xl text-4xl font-bold'>Standard</h2>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
-									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor
-									sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin
-									adipiscing amet. Euismod elit, vel tellus, sed in sapien..
-								</p>
-							</div>
-							<p>$40/mo.</p>
-						</div>
-						<div className='flex w-1/2 items-center gap-4 rounded-xl bg-white p-9 hover:cursor-pointer '>
-							<input type='checkbox' />
-							<div>
-								<h2 className='font-size p-1 text-xl text-4xl font-bold'>Pro</h2>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
-									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor
-									sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin
-									adipiscing amet. Euismod elit, vel tellus, sed in sapien..
-								</p>
-							</div>
-							<p>$100/mo.</p>
-            </div>
-            <Button className='w-1/2 bg-gradient-to-r from-brandBlue to-brandGreen'>
-            <p className='text-xl text-white'>
-              Continue
+const Register: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-vendorfiedBlue/[.1] to-vendorfiedGreen/[.1] rounded-3xl">
+      <div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        {/* Grid Layout */}
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:gap-20">
+          {/* column 1 */}
+          <div className="space-y-6 md:my-44">
+            <h1 className="text-4xl font-bold text-brandBlue sm:text-4xl">
+              Welcome
+            </h1>
+            <h2 className="text-2xl font-bold sm:text-5xl">
+              Select your Vendorfied plan
+            </h2>
+            <p className="text-base text-gray-500 sm:text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed
+              justo, sed fermentum ornare proin adipiscing amet. Euismod elit,
+              vel tellus, sed in sapien. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Cras sed justo, sed fermentum ornare proin
+              adipiscing amet. Euismod elit, vel tellus, sed in sapien.
             </p>
-            </Button>
+            <div className="space-y-4">
+              <details>
+                <summary
+                 
+                  className="flex items-center justify-evenly px-4 py-2 text-xl font-bold cursor-pointer"
+                >
+                  <span className='px-3 text-xl' onClick={(e) => e.preventDefault()}>
+									{open ? '-' : '+'}
+								</span>
+                  <span>How do I cancel my subscription?</span>
+                </summary>
+                <div className="px-4 py-2">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </details>
+              <details>
+                <summary
+                  className="flex items-center justify-evenly px-4 py-2 text-xl font-bold cursor-pointer"
+                >
+                  <span className='px-3 text-xl' onClick={(e) => e.preventDefault()}>
+									{open ? '-' : '+'}
+								</span>
+                  <span>How do I cancel my subscription?</span>
+                </summary>
+                <div className="px-4 py-2">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </details>
+              <details>
+                <summary
+                  className="flex items-center justify-evenly px-4 py-2 text-xl font-bold cursor-pointer"
+                >
+                  <span className='px-3 text-xl' onClick={(e) => e.preventDefault()}>
+									{open ? '-' : '+'}
+								</span>
+                  <span>How do I cancel my subscription?</span>
+                </summary>
+                <div className="px-4 py-2">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </details>
+            </div>
+          </div>
+          {/* column 2 */}
+          <div className="space-y-6 md:space-y-8 ">
+            <div className="bg-white shadow rounded-lg">
+              <div className="px-6 py-8">
+                <div className="flex items-center mb-4">
+                  <input type="checkbox" className="w-5 h-5 mr-4" />
+                  <h3 className="text-4xl font-bold">Starter</h3>
+                </div>
+                <p className="text-sm text-gray-500">
+                  Loremipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
+									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor
+									sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin
+									adipiscing amet. Euismod elit, vel tellus, sed in sapien..
+								</p>
+							<p>$20/mo.</p>
+							</div>
+						</div>
+            <div className="bg-white shadow rounded-lg">
+              <div className="px-6 py-8">
+                <div className="flex items-center mb-4">
+                  <input type="checkbox" className="w-5 h-5 mr-4" />
+                  <h3 className="text-4xl font-bold">Standard</h3>
+                </div>
+                <p className="text-sm text-gray-500">
+                  Loremipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
+									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor
+									sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin
+									adipiscing amet. Euismod elit, vel tellus, sed in sapien..
+								</p>
+							<p>$40/mo.</p>
+							</div>
+						</div>
+            <div className="bg-white shadow rounded-lg">
+              <div className="px-6 py-8">
+                <div className="flex items-center mb-4">
+                  <input type="checkbox" className="w-5 h-5 mr-4" />
+                  <h3 className="text-4xl font-bold">Pro</h3>
+                </div>
+                <p className="text-sm text-gray-500">
+                  Loremipsum dolor sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum
+									ornare proin adipiscing amet. Euismod elit, vel tellus, sed in sapien. Lorem ipsum dolor
+									sit amet, consectetur adipiscing elit. Cras sed justo, sed fermentum ornare proin
+									adipiscing amet. Euismod elit, vel tellus, sed in sapien..
+								</p>
+							<p>$100/mo.</p>
+							</div>
+						</div>
+						<Button className='w-full bg-gradient-to-r from-brandBlue to-brandGreen'>
+							<p className='text-xl text-white'>Continue</p>
+						</Button>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
